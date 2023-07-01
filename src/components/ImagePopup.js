52ac68bf9a2808dc49882a636cadab17208c8dfe
sharkavli <saidovavli@gmail.com>
2023-07-01 19:53:31 +0500

    review changes 1.1

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,14 +1,17 @@
 function ImagePopup(props) {
   const { name, link } = props.card;
   return (
-    <div className={`popup ${name ? 'popup_opened' : ''}`} id="popupImg">
+    <div
+      className={`popup popup__image ${name ? 'popup_opened' : ''}`}
+      id="popupImg"
+    >
       <div className="popup__photo-container">
         <div
           onClick={props.onClose}
           className="popup__close"
           id="closeImg"
         ></div>
-        <img alt={name} src={link} className="popup__image" />
+        <img alt={name} src={link} className="popup__picture" />
         <p className="popup__text">{name}</p>
       </div>
     </div>
